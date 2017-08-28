797c58a3789930e9cbe913fce27dc990174e8596
Guillaume Chau <alphadelta.fg@gmail.com>
2017-08-28 15:58:45 +0200

    Improved rollup config

--- rollup.config.browser.js
+++ rollup.config.browser.js
@@ -1,25 +1,13 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import base from './rollup.config.base'
 import uglify from 'rollup-plugin-uglify'
 import { minify } from 'uglify-js-harmony'
 
-export default {
-  entry: 'src/index.js',
+const config = Object.assign({}, base, {
   dest: 'dist/vue-apollo.min.js',
   format: 'iife',
-  exports: 'named',
   moduleName: 'VueApollo',
-  plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    uglify({}, minify),
-  ],
-}
+})
+
+config.plugins.push(uglify({}, minify))
+
+export default config
--- rollup.config.es.js
+++ rollup.config.es.js
@@ -1,22 +1,8 @@
-import commonjs from 'rollup-plugin-commonjs'
-import resolve from 'rollup-plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
+import base from './rollup.config.base'
 
-export default {
-  entry: 'src/index.js',
+const config = Object.assign({}, base, {
   dest: 'dist/vue-apollo.esm.js',
   format: 'es',
-  exports: 'named',
-  moduleName: 'vue-apollo',
-  plugins: [
-    resolve({
-      jsnext: true,
-      main: true,
-      browser: true,
-    }),
-    commonjs(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
-}
+})
+
+export default config
