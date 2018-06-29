adfc158d50530b35edb00c7f28b5b54b9d0c38b9
Guillaume Chau <alphadelta.fg@gmail.com>
2018-06-29 15:39:20 +0200

    fix: websocket context

--- tests/demo/apollo-server/context.js
+++ tests/demo/apollo-server/context.js
@@ -5,18 +5,23 @@ const users = require('./connectors/users')
 // connection => Subscription
 // eslint-disable-next-line no-unused-vars
 module.exports = ({ req, connection }) => {
-  console.log('connection:', connection)
+  // If the websocket context was already resolved
+  if (connection && connection.context) return connection.context
+
   let rawToken
+  // HTTP
   if (req) rawToken = req.get('Authorization')
+  // Websocket
   if (connection) rawToken = connection.authorization
+
+  // Token
   const token = rawToken ? JSON.parse(rawToken) : null
   let userId
 
+  // User validation
   if (token && users.validateToken(token)) {
     userId = token.userId
   }
-
-  console.log('token:', token, 'userId:', userId)
 
   return {
     token,
--- tests/demo/apollo-server/directives/private.js
+++ tests/demo/apollo-server/directives/private.js
@@ -5,7 +5,6 @@ module.exports = class PrivateDirective extends SchemaDirectiveVisitor {
   visitFieldDefinition (field) {
     const { resolve = defaultFieldResolver } = field
     field.resolve = (root, args, context, info) => {
-      console.log(info.fieldName, context.userId)
       if (!context.userId) throw new Error('Unauthorized')
       return resolve(root, args, context, info)
     }
