e7c8e6821bd89abfbf9541a60e4db13abe898083
Lachlan Stuart <Lachlan@LachlanStuart.com>
2018-08-23 11:46:58 +0200

    Add TypeScript interface to $apollo

--- types/vue-apollo.d.ts
+++ types/vue-apollo.d.ts
@@ -1,15 +1,9 @@
 import Vue, { PluginObject, PluginFunction } from 'vue';
-import { DocumentNode } from 'graphql';
-import { ApolloClient } from 'apollo-client';
-import { SubscriptionOptions, ObservableQuery } from 'apollo-client'
-import { DataProxy } from 'apollo-cache';
-import { subscribe } from 'graphql/subscription/subscribe';
+import { ApolloClient, ObservableQuery } from 'apollo-client';
 import { ApolloProvider, VueApolloComponent } from './apollo-provider'
 import {
   VueApolloQueryOptions,
-  VueApolloMutationOptions,
   VueApolloSubscriptionOptions,
-  ApolloVueThisType,
   VueApolloOptions,
   WatchLoading,
   ErrorHandler
@@ -28,15 +22,46 @@ export class VueApollo extends ApolloProvider implements PluginObject<{}>{
   static install(pVue: typeof Vue, options?:{} | undefined): void;
 }
 
-type Query<V> = (key: string, options: VueApolloQueryOptions<V, any>) => void;
-type Mutate<V, R=any> = <R=any>(params: VueApolloMutationOptions<V, R>) => Promise<R>;
-type Subscribe<R=any> = <R=any>(params: SubscriptionOptions) => ObservableQuery<R>;
-export interface ApolloProperty<V> {
-  [key: string]: Query<V> | Mutate<V> | Subscribe; // smart query
-  queries: any;
-  mutate: Mutate<V>;
-  subscribe: Subscribe;
-  addSmartQuery: Query<V>;
+interface SmartApollo<V, R=any> {
+  skip: boolean;
+  refresh(): void;
+  start(): void;
+  stop(): void;
+}
+
+export interface SmartQuery<V, R=any> extends SmartApollo<V, R> {
+  loading: boolean;
+  fetchMore: ObservableQuery<any>['fetchMore'];
+  subscribeToMore: ObservableQuery<any>['subscribeToMore'];
+  refetch: ObservableQuery<any>['refetch'];
+  setVariables: ObservableQuery<any>['setVariables'];
+  setOptions: ObservableQuery<any>['setOptions'];
+  startPolling: ObservableQuery<any>['startPolling'];
+  stopPolling: ObservableQuery<any>['stopPolling'];
+}
+
+export interface SmartSubscription<V, R=any> extends SmartApollo<V, R> {
+}
+
+export interface DollarApollo<V> {
+  vm: V;
+  queries: Record<string, SmartQuery<V, any>>;
+  subscriptions: Record<string, SmartSubscription<V, any>>;
+  client: ApolloClient<{}>;
+  readonly provider: ApolloProvider;
+  readonly loading: boolean;
+
+  // writeonly not yet implemented in TypeScript: https://github.com/Microsoft/TypeScript/issues/21759
+  /* writeonly */ skipAllQueries: boolean;
+  /* writeonly */ skipAllSubscriptions: boolean;
+  /* writeonly */ skipAll: boolean;
+
+  query: ApolloClient<{}>['query'];
+  mutate: ApolloClient<{}>['mutate'];
+  subscribe: ApolloClient<{}>['subscribe'];
+
+  addSmartQuery<R=any>(key: string, options: VueApolloQueryOptions<V, R>): SmartQuery<V, R>;
+  addSmartSubscription<R=any>(key: string, options: VueApolloSubscriptionOptions<V, R>): SmartSubscription<V, R>;
 }
 
 export function willPrefetch (component: VueApolloComponent, contextCallback?: boolean): VueApolloComponent
