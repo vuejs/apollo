b5b4dab1dd4307a68b3eb758e2da0cd6c7b966f8
Yumin Xia <me@yumin-xia.com>
2019-02-14 11:40:28 -0800

    export apollo options for extending

--- dist/vue-apollo.esm.js
+++ dist/vue-apollo.esm.js
@@ -225,94 +225,104 @@ function createCommonjsModule(fn, module) {
  *
  * @return {Function}  A new, throttled, function.
  */
-function throttle ( delay, noTrailing, callback, debounceMode ) {
+function throttle (delay, noTrailing, callback, debounceMode) {
+  /*
+   * After wrapper has stopped being called, this timeout ensures that
+   * `callback` is executed at the proper times in `throttle` and `end`
+   * debounce modes.
+   */
+  var timeoutID;
+  var cancelled = false; // Keep track of the last time `callback` was executed.
 
-	/*
-	 * After wrapper has stopped being called, this timeout ensures that
-	 * `callback` is executed at the proper times in `throttle` and `end`
-	 * debounce modes.
-	 */
-	var timeoutID;
+  var lastExec = 0; // Function to clear existing timeout
 
-	// Keep track of the last time `callback` was executed.
-	var lastExec = 0;
+  function clearExistingTimeout() {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+  } // Function to cancel next exec
 
-	// `noTrailing` defaults to falsy.
-	if ( typeof noTrailing !== 'boolean' ) {
-		debounceMode = callback;
-		callback = noTrailing;
-		noTrailing = undefined;
-	}
 
-	/*
-	 * The `wrapper` function encapsulates all of the throttling / debouncing
-	 * functionality and when executed will limit the rate at which `callback`
-	 * is executed.
-	 */
-	function wrapper () {
+  function cancel() {
+    clearExistingTimeout();
+    cancelled = true;
+  } // `noTrailing` defaults to falsy.
 
-		var self = this;
-		var elapsed = Number(new Date()) - lastExec;
-		var args = arguments;
 
-		// Execute `callback` and update the `lastExec` timestamp.
-		function exec () {
-			lastExec = Number(new Date());
-			callback.apply(self, args);
-		}
+  if (typeof noTrailing !== 'boolean') {
+    debounceMode = callback;
+    callback = noTrailing;
+    noTrailing = undefined;
+  }
+  /*
+   * The `wrapper` function encapsulates all of the throttling / debouncing
+   * functionality and when executed will limit the rate at which `callback`
+   * is executed.
+   */
 
-		/*
-		 * If `debounceMode` is true (at begin) this is used to clear the flag
-		 * to allow future `callback` executions.
-		 */
-		function clear () {
-			timeoutID = undefined;
-		}
 
-		if ( debounceMode && !timeoutID ) {
-			/*
-			 * Since `wrapper` is being called for the first time and
-			 * `debounceMode` is true (at begin), execute `callback`.
-			 */
-			exec();
-		}
+  function wrapper() {
+    var self = this;
+    var elapsed = Date.now() - lastExec;
+    var args = arguments;
 
-		// Clear any existing timeout.
-		if ( timeoutID ) {
-			clearTimeout(timeoutID);
-		}
+    if (cancelled) {
+      return;
+    } // Execute `callback` and update the `lastExec` timestamp.
 
-		if ( debounceMode === undefined && elapsed > delay ) {
-			/*
-			 * In throttle mode, if `delay` time has been exceeded, execute
-			 * `callback`.
-			 */
-			exec();
 
-		} else if ( noTrailing !== true ) {
-			/*
-			 * In trailing throttle mode, since `delay` time has not been
-			 * exceeded, schedule `callback` to execute `delay` ms after most
-			 * recent execution.
-			 *
-			 * If `debounceMode` is true (at begin), schedule `clear` to execute
-			 * after `delay` ms.
-			 *
-			 * If `debounceMode` is false (at end), schedule `callback` to
-			 * execute after `delay` ms.
-			 */
-			timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
-		}
+    function exec() {
+      lastExec = Date.now();
+      callback.apply(self, args);
+    }
+    /*
+     * If `debounceMode` is true (at begin) this is used to clear the flag
+     * to allow future `callback` executions.
+     */
 
-	}
 
-	// Return the wrapper function.
-	return wrapper;
+    function clear() {
+      timeoutID = undefined;
+    }
 
+    if (debounceMode && !timeoutID) {
+      /*
+       * Since `wrapper` is being called for the first time and
+       * `debounceMode` is true (at begin), execute `callback`.
+       */
+      exec();
+    }
+
+    clearExistingTimeout();
+
+    if (debounceMode === undefined && elapsed > delay) {
+      /*
+       * In throttle mode, if `delay` time has been exceeded, execute
+       * `callback`.
+       */
+      exec();
+    } else if (noTrailing !== true) {
+      /*
+       * In trailing throttle mode, since `delay` time has not been
+       * exceeded, schedule `callback` to execute `delay` ms after most
+       * recent execution.
+       *
+       * If `debounceMode` is true (at begin), schedule `clear` to execute
+       * after `delay` ms.
+       *
+       * If `debounceMode` is false (at end), schedule `callback` to
+       * execute after `delay` ms.
+       */
+      timeoutID = setTimeout(debounceMode ? clear : exec, debounceMode === undefined ? delay - elapsed : delay);
+    }
+  }
+
+  wrapper.cancel = cancel; // Return the wrapper function.
+
+  return wrapper;
 }
 
 /* eslint-disable no-undefined */
-
 /**
  * Debounce execution of a function. Debouncing, unlike throttling,
  * guarantees that a function is only executed a single time, either at the
@@ -327,8 +337,9 @@ function throttle ( delay, noTrailing, callback, debounceMode ) {
  *
  * @return {Function} A new, debounced function.
  */
-function debounce ( delay, atBegin, callback ) {
-	return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false);
+
+function debounce (delay, atBegin, callback) {
+  return callback === undefined ? throttle(delay, atBegin, false) : throttle(delay, callback, atBegin !== false);
 }
 
 var index_esm = /*#__PURE__*/Object.freeze({
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,7 @@
 import './vue'
 import { VueApollo } from './vue-apollo'
 import { ApolloProvider } from './apollo-provider'
+import * as VueApolloOptions from './options'
 
 export default VueApollo
-export { ApolloProvider }
+export { ApolloProvider, VueApolloOptions }
