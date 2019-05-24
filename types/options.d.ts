31ee8ad6211857b40777fa8ee726bd0db7e8c08e
Harman Pannu <iharmanpannu@gmail.com>
2019-05-23 23:33:39 -0700

    Typescript compilation error with latest apollo package
    Fixed a bug

--- types/options.d.ts
+++ types/options.d.ts
@@ -47,7 +47,7 @@ export interface VueApolloQueryOptions<V, R> extends ExtendableVueApolloQueryOpt
 export interface VueApolloMutationOptions<V, R> extends MutationOptions<R> {
   mutation: DocumentNode;
   variables?: VariableFn<V>;
-  optimisticResponse?: ((this: ApolloVueThisType<V>) => any) | Object;
+  optimisticResponse?: ((this: ApolloVueThisType<V>) => any) | any
 }
 
 export interface VueApolloSubscriptionOptions<V, R> extends SubscriptionOptions {
