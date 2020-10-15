72d7402eb29893a7e63fc675a8057a8f5a4a825f
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-10-15 19:03:00 +0200

    fix(useApolloClient): add id arg to resolveClient

--- packages/vue-apollo-composable/src/useApolloClient.ts
+++ packages/vue-apollo-composable/src/useApolloClient.ts
@@ -4,13 +4,13 @@ import ApolloClient from 'apollo-client'
 export const DefaultApolloClient = Symbol('default-apollo-client')
 export const ApolloClients = Symbol('apollo-clients')
 
-export interface UseApolloClientReturn<TCacheShape> {
-  resolveClient: (clientId?: string) => ApolloClient<TCacheShape>
-  readonly client: ApolloClient<TCacheShape>
-}
-
 type ClientId = string
 type ClientDict<T> = Record<ClientId, ApolloClient<T>>
+
+export interface UseApolloClientReturn<TCacheShape> {
+  resolveClient: (clientId?: ClientId) => ApolloClient<TCacheShape>
+  readonly client: ApolloClient<TCacheShape>
+}
 
 function resolveDefaultClient<T>(providedApolloClients: ClientDict<T>, providedApolloClient: ApolloClient<T>): ApolloClient<T> {
   const resolvedClient = providedApolloClients ?
@@ -37,8 +37,8 @@ export function useApolloClient<TCacheShape = any>(clientId?: ClientId): UseApol
   const providedApolloClients: ClientDict<TCacheShape> = inject(ApolloClients, null)
   const providedApolloClient: ApolloClient<TCacheShape> = inject(DefaultApolloClient, null)
 
-  function resolveClient() {
-    if (clientId) {
+  function resolveClient(id: ClientId = clientId) {
+    if (id) {
       return resolveClientWithId(providedApolloClients, clientId)
     }
     return resolveDefaultClient(providedApolloClients, providedApolloClient)
