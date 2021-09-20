a76d25901b5ad602c38f7be1ae51acb7c34361c4
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-09-20 20:32:53 +0200

    test(e2e): fix error policy assertion

--- packages/test-e2e/tests/e2e/specs/errorPolicy.js
+++ packages/test-e2e/tests/e2e/specs/errorPolicy.js
@@ -3,15 +3,15 @@ describe('errorPolicy', () => {
     cy.visit('/partial-error')
 
     // All
-    cy.get('.all .result').should('contain', '{"good":"good","bad":null}')
+    cy.get('.all .result').should('contain', '{"bad":null,"good":"good"}')
     cy.get('.all .error').should('contain', 'Error: GraphQL error: An error')
 
     // None
-    cy.get('.none .result').should('not.contain', '{"good":"good","bad":null}')
+    cy.get('.none .result').should('not.contain', 'good')
     cy.get('.none .error').should('contain', 'Error: GraphQL error: An error')
 
     // Ignore
-    cy.get('.ignore .result').should('contain', '{"good":"good","bad":null}')
+    cy.get('.ignore .result').should('contain', '{"bad":null,"good":"good"}')
     cy.get('.ignore .error').should('not.exist')
   })
 })
