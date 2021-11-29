8f5c347b82c2af1dcd4532eb8ea8e6b61763d43a
Guillaume Chau <guillaume.b.chau@gmail.com>
2021-11-29 12:06:27 +0100

    chore: use @akryum/gen-changelog

--- scripts/update-version.mjs
+++
@@ -1,5 +0,0 @@
-import fs from 'fs'
-const lernaConfig = JSON.parse(fs.readFileSync('lerna.json', { encoding: 'utf8' }))
-const pkgData = JSON.parse(fs.readFileSync('package.json', { encoding: 'utf-8' }))
-pkgData.version = lernaConfig.version
-fs.writeFileSync('package.json', JSON.stringify(pkgData, null, 2) + '\n', { encoding: 'utf-8' })
